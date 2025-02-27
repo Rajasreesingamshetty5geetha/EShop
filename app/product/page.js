938c58d568a744f38ffe6301da6ae12944129b19
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2025-02-27 15:04:16 +0530

    page.js

--- app/product/page.js
+++ app/product/page.js
@@ -1,11 +1,16 @@
 "use client"; 
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import ProductCard from "../components/Product/ProductCard"; 
 
 const ProductPage = () => {
   return (
     <div>
       <ProductContent />
+      <Suspense fallback={<div>Loading Product...</div>}>
+        <ProductCard />
+      </Suspense>
     </div>
   );
 };
