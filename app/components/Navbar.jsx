abc668d82468cf49126238df78bd99fb94afa000
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2025-02-28 16:48:49 +0530

    Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -136,7 +136,7 @@ export default function Navbar() {
 
 
   return (
-    <div className="bg-white sticky ">
+    <div className="bg-white  ">
       {/* Mobile menu */}
       <Dialog open={open} onClose={setOpen} className="relative z-40 lg:hidden">
         <DialogBackdrop
