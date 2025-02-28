4544439b93776ba403b7101c9bc204966f53ab11
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2025-02-28 14:22:51 +0530

    Delete app/components/Product directory

--- app/components/Product/SidebarData.js
+++
@@ -1,9 +0,0 @@
-export const data = {
-    ColorOptions: ['Beige', 'Black', 'Blue', 'Yellow', 'White', 'Dark Blue'],
-    MoreOptions: ["Orange", "Pink", "Green", "Dark Green"],
-    SizeOptions: ["S", "M", "L", "XL", "XXL"],
-    options: ["100", "200", "300", "400", "500", "600"],
-    Discount: ["30% or More","40% or More","50% or More","60% or More","70% or More"],
-    SortOptions: ["Price(High to Low)","Price(Low to High)","Ratings","New Arrivals"]
-};
-  
