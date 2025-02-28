c1679441bfb19b77dd155c0e67a76456d6a58b7b
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2025-02-28 14:58:24 +0530

    Homecarousel.jsx

--- app/components/Home/Homecarousel.jsx
+++ app/components/Home/Homecarousel.jsx
@@ -3,9 +3,10 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { HomecarouselData } from "./HomecarouselData";
 
+
 const Homecarousel = () => {
   return (
-    <div className="w-full block bg-white mt-2 mx-auto max-w-[1400px] overflow-hidden">
+    <div className="w-full bg-white mt-2 mx-auto max-w-[1600px] overflow-hidden">
       <Carousel
         autoPlay
         infiniteLoop
@@ -17,16 +18,11 @@ const Homecarousel = () => {
         showArrows={false}
       >
         {HomecarouselData.map((item, index) => (
-          <a
-            key={index}
-            href={item.href}
-            className="block"
-            
-          >
+          <a key={index} href={item.href} className="block">
             <img
               alt={item.imageAlt}
               src={item.imageSrc}
-              className="w-full max-h-[90vh] object-cover rounded-lg p-4"
+              className="w-full h-auto max-h-[500px] md:max-h-[600px] lg:max-h-[700px] object-cover rounded-lg"
             />
           </a>
         ))}
