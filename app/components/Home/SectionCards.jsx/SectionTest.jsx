ac5c39d0591383fed6827ce55f86e125ed0e1479
Rajasree singamsetty <88222096+Rajasreesingamshetty5geetha@users.noreply.github.com>
2025-02-28 14:25:32 +0530

    SectionTest.jsx

--- app/components/Home/SectionCards.jsx/SectionTest.jsx
+++ app/components/Home/SectionCards.jsx/SectionTest.jsx
@@ -1,7 +1,7 @@
 import { ArrowLeft, ArrowRight } from '@mui/icons-material';
-import React, { useEffect, useState } from 'react';
-import { useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { sections } from './SectionCardData';
+import { useRouter } from 'next/navigation';
 
 export default function SectionTest() {
     return (
@@ -19,6 +19,11 @@ export default function SectionTest() {
 const SectionSlider = ({ products }) => {
     const containerRef = useRef(null);
     const [isScrolled, setIsScrolled] = useState(false);
+    const router = useRouter();
+
+    const handleProductClick = (product) => {
+        router.push(`/product-overview?id=${product.id}`);
+    };
 
     const scroll = (direction) => {
         if (containerRef.current) {
@@ -50,13 +55,11 @@ const SectionSlider = ({ products }) => {
 
     return (
         <div className='relative w-full px-12 py-6'>
-
             {/* Left Arrow */}
             {isScrolled && (
                 <button
                     onClick={() => scroll("left")}
-                    className='absolute ml-3 top-1/2 -translate-y-1/2 bg-gradient-to-r from-gray-800 to-gray-800 text-white 
-                      p-2 rounded-full shadow-lg shadow-white/50 z-10 sm:block md:block'
+                    className='absolute ml-3 top-1/2 -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg z-10 sm:block md:block'
                 >
                     <ArrowLeft size={24} />
                 </button>
@@ -68,13 +71,16 @@ const SectionSlider = ({ products }) => {
                 className="flex overflow-hidden gap-4 scroll-smooth py-10 px-8 bg-pink-500 rounded-sm"
             >
                 {products.map((product) => (
-                    <div key={product.id} className='min-w-[200px] w-[200px] h-[300px] bg-white p-4 rounded-lg shadow-md object-top cursor-pointer flex flex-col justify-center '>
+                    <div
+                        key={product.id}
+                        className='min-w-[200px] w-[200px] h-[300px] bg-white p-4 rounded-lg shadow-md  flex flex-col justify-center hover:scale-105 hover:shadow-2xl transition-transform duration-300'
+                    >
                         <img
                             src={product.image}
                             alt={product.brand}
-                            className='w-full h-full object-cover rounded-md aspect-square object-top'
+                            className='w-full h-full object-cover rounded-md aspect-square object-top hover:opacity-95 opacity-85 transition-opacity duration-300'
                         />
-                        <div className='flex flex-grow flex-col justify-end hover:h-96 hover:shadow-md hover:rounded-b-lg'>
+                        <div className='flex flex-grow flex-col justify-end'>
                             <h3 className='text-lg font-medium text-gray-900 mt-2'>{product.brand}</h3>
                             <p className="desc">{product.desc}</p>
                         </div>
@@ -85,12 +91,10 @@ const SectionSlider = ({ products }) => {
             {/* Right Arrow */}
             <button
                 onClick={() => scroll("right")}
-                className='absolute lg:right-16 right-14 top-1/2 -translate-y-1/2 bg-gradient-to-r from-gray-800 to-gray-800 text-white 
-                  p-2 rounded-full shadow-lg shadow-white/50 z-10 sm:block md:block '
+                className='absolute lg:right-16 right-14 top-1/2 -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-lg z-10 sm:block md:block'
             >
                 <ArrowRight size={24} />
             </button>
-
         </div>
     );
 };
